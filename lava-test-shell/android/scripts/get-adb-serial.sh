7273242aa3cbdfe8018354412522ff2905639c2c
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2020-11-11 12:44:50 +0000

    lava-test-shell: fix get-adb-serial test
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>
    Change-Id: I5cb3294497919fac0e06685f1dafd793bf407b79

--- lava-test-shell/android/scripts/get-adb-serial.sh
+++ lava-test-shell/android/scripts/get-adb-serial.sh
@@ -1,13 +1,36 @@
 #!/bin/sh
 
+SKIP_INSTALL="true"
+
+usage() {
+    echo "Usage: $0 [-S <true|false>]" 1>&2
+    exit 1
+}
+
+while getopts ":S:s:t:l:n:d:u:g:" opt; do
+    case "${opt}" in
+        S) SKIP_INSTALL="${OPTARG}" ;;
+        *) usage ;;
+    esac
+done
+
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    echo "Package installation skipped"
+    lava-test-case install-base --result skip
+else
+    apt-get update -q
+    DEBIAN_FRONTEND=noninteractive lava-test-case install-base --shell apt-get -q -y install -o Dpkg::Options::="--force-confold" adb fastboot android-tools-adb android-tools-fastboot
+fi
+
 which adb
 # start daemon if not yet running.
 adb start-server || true
 adb wait-for-device
 echo
 # start adb and stop the daemon start message from appearing in $result
-adb get-serialno || true
-result=`adb get-serialno 2>&1 | tail -n1`
+lava-test-case adb-serial --shell adb get-serialno
+result=$(adb get-serialno 2>&1 | tail -n1)
 if [ "$result" = "unknown" ]; then
     echo "ERROR: adb get-serialno returned" $result
     exit 1
